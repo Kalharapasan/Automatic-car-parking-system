f32a6b8b8a167031aaa596fc598e5041c6c4567b
Pasan Kalhara <76808092+Kalharapasan@users.noreply.github.com>
2026-01-01 20:14:40 +0530

    Complete Digram

--- Automatic car parking system/src/main.cpp
+++ Automatic car parking system/src/main.cpp
@@ -9,6 +9,9 @@
 #define OUT_ECHO_PIN 5
 #define OUT_TRIG_PIN 4
 
+int outServoPin =9;
+int inServoPin =6;
+
 Servo outServo; 
 Servo inServo;
 
@@ -16,8 +19,14 @@ LiquidCrystal_I2C lcd(0x27,16,2);
 
 void setup(){
 
-  outServo.attach(9); 
-  inServo.attach();
+  outServo.attach(outServoPin); 
+  inServo.attach(inServoPin);
+
+  pinMode(IN_ECHO_PIN, INPUT);
+  pinMode(IN_TRIG_PIN,OUTPUT);
+
+  pinMode(OUT_ECHO_PIN, INPUT);
+  pinMode(OUT_TRIG_PIN,OUTPUT);
 
 }
 
